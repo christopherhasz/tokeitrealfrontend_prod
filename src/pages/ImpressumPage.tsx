aea3c25771457621d54697f98bdfafb84c7cd4d8
christopherhasz <christopher.hasz@icloud.com>
2025-11-02 18:02:04 +0100

    Updated ImpressumPage.tsx

--- src/pages/ImpressumPage.tsx
+++ src/pages/ImpressumPage.tsx
@@ -17,10 +17,10 @@ export const ImpressumPage: React.FC = () => {
         name: 'TokeItReal GbR',
         representatives: 'vertreten durch die Gesellschafter',
         founders: 'Christopher Haß & Felix Behnke',
-        address: '[Straße, Hausnummer]',
-        city: '[PLZ, Ort – voraussichtlich Berlin]',
+        address: 'Waldstraße 21',
+        city: '15345 Altlandsberg',
         country: 'Deutschland',
-        email: 'kontakt@tokeitreal.com',
+        email: 'info@tokeitreal.com',
         web: 'tokeitreal.com'
       },
       representationTitle: 'Vertretung',
